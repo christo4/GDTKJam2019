0fbef7b1abafb3bb5cd613abef00578717c29808
EverestWild <ShadowedOblivion@gmail.com>
2019-08-02 19:39:41 -0600

    Add files via upload

---
+++ GDTK Game Jam 2019/Blue.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_Blue_mouse_entered():
+	print("MOUSE IS INSIDE?")
+	pass # Replace with function body.
