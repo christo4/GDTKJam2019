f8c4e27f7c061cc541ee13d183b4ac04abec368a
christo4 <christopher.rodrigue@ucalgary.ca>
2019-08-03 15:57:58 -0600

    Yarn game

---
+++ GDTK Game Jam 2019/RedYarn2.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var entered
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	entered = false
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("ui_left_mouse") and entered and get_parent().yarn2 == true:
+		print("win")
+	if Input.is_action_just_pressed("ui_left_mouse") and entered and get_parent().yarn2 != true:
+		print("lose")
+
+
+func _on_BlueYarn_mouse_entered():
+	entered = true
+
+
+func _on_BlueYarn_mouse_exited():
+	entered = false
