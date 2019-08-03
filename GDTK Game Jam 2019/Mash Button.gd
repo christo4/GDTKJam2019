cb415ac2831587069f5d823d84636fd786d7644a
Everest Wild <EverestWild@gmail.com>
2019-08-03 16:09:07 -0600

    Button Mash done
    - Button Mash Done
    - Camera and main scene started

--- GDTK Game Jam 2019/Mash Button.gd
+++ GDTK Game Jam 2019/Mash Button.gd
@@ -1,14 +1,29 @@
 extends Area2D
 
 var entered
+var marker
 
 func _ready():
 	entered = false
+	marker = get_parent().get_node("Marker")
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_left_mouse") and entered:
-		print("You clicked it!")
-	$Sprite.play("Idle")
+		if marker.position.x < 1472:
+			marker.position.x += 32
+	elif Input.is_action_pressed("ui_left_mouse") and entered:
+		$Sprite.play("Pressed")
+	else:
+		$Sprite.play("Idle")
+	if marker.position.x > 448 and marker.position.x < 960:
+		marker.position.x -= 1
+	elif marker.position.x >= 960 and marker.position.x < 1216:
+		marker.position.x -= 2
+	elif marker.position.x >= 1216 and marker.position.x < 1472:
+		marker.position.x -= 3
+	elif marker.position.x >= 1472:
+		print("YOU WIN!")
+		get_tree().quit()
 
 func _on_Mash_Button_mouse_entered():
 	entered = true
