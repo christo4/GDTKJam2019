f42dbd9618380292ad07605b9a9a5026b8af2c27
christo4 <christopher.rodrigue@ucalgary.ca>
2019-08-03 23:40:54 -0600

    scissor game

--- GDTK Game Jam 2019/Hand.gd
+++ GDTK Game Jam 2019/Hand.gd
@@ -3,17 +3,23 @@ var mouse_position
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var scissorNode = get_parent().get_node("Scissor")
-	scissorNode.connect("area_entered", self, "hit_scissor")
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)  #hides the cursor
+
+	mouse_position = get_viewport().get_mouse_position();
+	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)  #hides the cursor
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 
 func _process(delta):
 	mouse_position = get_viewport().get_mouse_position();
+	#print(str(mouse_position.x) + "," + str(mouse_position.y))
 	position = mouse_position
 	
 	
-func hit_scissor(object_collided):
-	print("Hit scissor")
+func hit_scissor(object_collided):	
+	
+	if(object_collided.get_name() == "Hand"):
+		print("you lose")
+	else:
+		print("you good")
 
 
